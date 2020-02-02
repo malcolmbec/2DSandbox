e4ed415f64df257d37d5c71d349521c4014ebcad
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2020-02-01 19:34:43 -0500

    Added camera follow script

---
+++ 2DSandbox/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -10f);
+
+    void Update()
+    {
+        // follow the player with offset
+        float new_x = player.position.x + offset.x;
+        float new_y = player.position.y + offset.y;
+        transform.position = new Vector3(new_x, new_y, offset.z);
+    }
+}
