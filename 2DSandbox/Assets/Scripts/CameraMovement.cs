d15b2fb74a8a0afd6d64f614fc825647f21d768c
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2020-02-01 19:44:31 -0500

    Set minimum camera height

--- 2DSandbox/Assets/Scripts/CameraMovement.cs
+++ 2DSandbox/Assets/Scripts/CameraMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private float minHeight = 0f;
     [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -10f);
 
     void Update()
@@ -12,6 +13,11 @@ public class CameraMovement : MonoBehaviour
         // follow the player with offset
         float new_x = player.position.x + offset.x;
         float new_y = player.position.y + offset.y;
+        // set minimum height for the camera
+        if (new_y < minHeight)
+        {
+            new_y = minHeight;
+        }
         transform.position = new Vector3(new_x, new_y, offset.z);
     }
 }
